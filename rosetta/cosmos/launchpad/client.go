809c24ff4e078fffa3a16bc9fa72f147eff7d5b1
Jonathan Gimeno <jgimeno@gmail.com>
2020-08-17 17:06:17 +0200

    temp commit context switch

--- rosetta/cosmos/launchpad/client.go
+++ rosetta/cosmos/launchpad/client.go
@@ -1,1 +1,31 @@
 package launchpad
+
+import (
+	"context"
+	"net/http"
+
+	cosmosclient "github.com/tendermint/cosmos-rosetta-gateway/rosetta/cosmos/launchpad/client/cosmos/generated"
+	tendermintclient "github.com/tendermint/cosmos-rosetta-gateway/rosetta/cosmos/launchpad/client/tendermint/generated"
+)
+
+type CosmosAPI struct {
+	Bank       CosmosBankAPI
+	Tendermint CosmosTendermintAPI
+}
+
+type CosmosBankAPI interface {
+	BankBalancesAddressGet(ctx context.Context, address string) (cosmosclient.InlineResponse2004, *http.Response, error)
+}
+
+type CosmosTendermintAPI interface {
+	NodeInfoGet(ctx context.Context) (cosmosclient.InlineResponse200, *http.Response, error)
+}
+
+type TendermintAPI struct {
+	Info TendermintInfoAPI
+}
+
+type TendermintInfoAPI interface {
+	NetInfo(ctx context.Context) (tendermintclient.NetInfoResponse, *http.Response, error)
+	Block(ctx context.Context, localVarOptionals *tendermintclient.BlockOpts) (tendermintclient.BlockResponse, *http.Response, error)
+}
--- rosetta/cosmos/launchpad/launchpad.go
+++ rosetta/cosmos/launchpad/launchpad.go
@@ -1,12 +1,7 @@
 package launchpad
 
 import (
-	"context"
-	"net/http"
-
 	"github.com/tendermint/cosmos-rosetta-gateway/rosetta"
-	cosmosclient "github.com/tendermint/cosmos-rosetta-gateway/rosetta/cosmos/launchpad/client/cosmos/generated"
-	tendermintclient "github.com/tendermint/cosmos-rosetta-gateway/rosetta/cosmos/launchpad/client/tendermint/generated"
 )
 
 type Launchpad struct {
@@ -14,28 +9,6 @@ type Launchpad struct {
 	tendermint TendermintAPI
 
 	properties rosetta.NetworkProperties
-}
-
-type CosmosAPI struct {
-	Bank       CosmosBankAPI
-	Tendermint CosmosTendermintAPI
-}
-
-type CosmosBankAPI interface {
-	BankBalancesAddressGet(ctx context.Context, address string) (cosmosclient.InlineResponse2004, *http.Response, error)
-}
-
-type CosmosTendermintAPI interface {
-	NodeInfoGet(ctx context.Context) (cosmosclient.InlineResponse200, *http.Response, error)
-}
-
-type TendermintAPI struct {
-	Info TendermintInfoAPI
-}
-
-type TendermintInfoAPI interface {
-	NetInfo(ctx context.Context) (tendermintclient.NetInfoResponse, *http.Response, error)
-	Block(ctx context.Context, localVarOptionals *tendermintclient.BlockOpts) (tendermintclient.BlockResponse, *http.Response, error)
 }
 
 func NewLaunchpad(tendermint TendermintAPI, cosmos CosmosAPI, properties rosetta.NetworkProperties) rosetta.Adapter {
