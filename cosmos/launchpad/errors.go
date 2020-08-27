59b11bc1a38a20303acf11faf891edfb22b6d53b
sahith-narahari <sahithnarahari@gmail.com>
2020-08-27 14:06:53 +0530

    Verify curve

--- cosmos/launchpad/errors.go
+++ cosmos/launchpad/errors.go
@@ -3,8 +3,9 @@ package launchpad
 import "github.com/tendermint/cosmos-rosetta-gateway/rosetta"
 
 var (
-	ErrInterpreting    = rosetta.NewError(1, "error interpreting data from node")
-	ErrNodeConnection  = rosetta.NewError(1, "error getting data from node")
-	ErrTxInterpreation = rosetta.NewError(2, "error reading tx data")
-	ErrTxUnmarshal     = rosetta.NewError(3, "error unmarshalling tx data")
+	ErrInterpreting     = rosetta.NewError(1, "error interpreting data from node")
+	ErrNodeConnection   = rosetta.NewError(1, "error getting data from node")
+	ErrTxInterpreation  = rosetta.NewError(2, "error reading tx data")
+	ErrTxUnmarshal      = rosetta.NewError(3, "error unmarshalling tx data")
+	ErrUnsupportedCurve = rosetta.NewError(4, "unsupported curve, expected secp256k1")
 )
