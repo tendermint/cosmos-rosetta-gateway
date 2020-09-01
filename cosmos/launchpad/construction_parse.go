fbcec51f2ea9b08efa232970fdc89fc421d49e72
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2020-09-01 17:33:28 +0300

    fix parsing tx & add operations
    about parsing the tx:
    codec.UnmarshalBinaryLengthPrefixed() seems like struct types but there
    is also codec.UnmarshalJSON() avaiable. I've used json.Unmarshal() at
    the end to be able to unmarshal to the types in the generated cosmos-sdk
    client so we could reuse toOperations().

--- cosmos/launchpad/construction_parse.go
+++ cosmos/launchpad/construction_parse.go
@@ -3,11 +3,11 @@ package launchpad
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 
 	"github.com/coinbase/rosetta-sdk-go/types"
-	"github.com/cosmos/cosmos-sdk/simapp"
-	"github.com/cosmos/cosmos-sdk/x/auth"
 
+	cosmosclient "github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk/generated"
 	"github.com/tendermint/cosmos-rosetta-gateway/rosetta"
 )
 
@@ -17,32 +17,22 @@ const (
 
 // ConstructionParse implements the /construction/parse endpoint.
 func (l Launchpad) ConstructionParse(ctx context.Context, request *types.ConstructionParseRequest) (*types.ConstructionParseResponse, *types.Error) {
-	var stdTx auth.StdTx
+	var stdTx cosmosclient.StdTx
 
 	// TODO: handle both base64 and hex
 	rawTx, err := base64.StdEncoding.DecodeString(request.Transaction)
 	if err != nil {
 		return nil, rosetta.NewError(5, err.Error())
 	}
-
-	codec := simapp.MakeCodec()
-	err = codec.UnmarshalBinaryLengthPrefixed(rawTx, &stdTx)
-	if err != nil {
+	if err := json.Unmarshal(rawTx, &stdTx); err != nil {
 		return nil, rosetta.NewError(5, err.Error())
 	}
 
-	var signers []string
-	addrs := stdTx.GetSigners()
-	for i := range addrs {
-		signers = append(signers, addrs[i].String())
-	}
-
-	// TODO: Convert msgs to operations
 	res := &types.ConstructionParseResponse{
-		Operations: nil,
-		Signers:    signers,
+		Operations: toOperations(stdTx.Value.Msg),
+		//Signers:    signers,
 		Metadata: map[string]interface{}{
-			Memo: stdTx.Memo,
+			//Memo: stdTx.Memo,
 		},
 	}
 	return res, nil
