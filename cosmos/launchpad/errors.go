527aefe95945aa899e175584eeccf5e3983c4a07
sahith-narahari <sahithnarahari@gmail.com>
2020-09-07 19:43:41 +0530

    Update errors

--- cosmos/launchpad/errors.go
+++ cosmos/launchpad/errors.go
@@ -4,10 +4,8 @@ import "github.com/tendermint/cosmos-rosetta-gateway/rosetta"
 
 var (
 	ErrInterpreting     = rosetta.NewError(1, "error interpreting data from node")
-	ErrNodeConnection   = rosetta.NewError(1, "error getting data from node")
-	ErrUnsupportedCurve = rosetta.NewError(2, "unsupported curve, expected secp256k1")
-	ErrInvalidRequest   = rosetta.NewError(3, "invalid request")
-	ErrUnsupportedOperation = rosetta.NewError(3, "unsupported operation")
-	ErrInvalidAddress       = rosetta.NewError(4, "invalid address")
-	ErrInvalidOperations    = rosetta.NewError(5, "invalid operations")
+	ErrNodeConnection   = rosetta.NewError(2, "error getting data from node")
+	ErrUnsupportedCurve = rosetta.NewError(3, "unsupported curve, expected secp256k1")
+	ErrInvalidRequest   = rosetta.NewError(4, "invalid request")
+	ErrInvalidAddress   = rosetta.NewError(6, "invalid address")
 )
