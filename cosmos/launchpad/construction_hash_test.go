2dbf55461c198943e6af96161f426de38553149d
Jonathan Gimeno <jgimeno@gmail.com>
2020-09-09 17:39:52 +0200

    add readfile

--- cosmos/launchpad/construction_hash_test.go
+++ cosmos/launchpad/construction_hash_test.go
@@ -4,25 +4,20 @@ import (
 	"context"
 	"encoding/base64"
 	"io/ioutil"
-	"os"
 	"testing"
 
 	"github.com/coinbase/rosetta-sdk-go/types"
-	"github.com/tendermint/cosmos-rosetta-gateway/rosetta"
-
 	"github.com/cosmos/cosmos-sdk/simapp"
-
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/stretchr/testify/require"
+
+	"github.com/tendermint/cosmos-rosetta-gateway/rosetta"
 )
 
 func TestLaunchpad_ConstructionHash(t *testing.T) {
 	expectedHash := "6f22ea7620ebcb5078d244f06e88dd26906ba1685135bfc34f83fefdd653198a"
 
-	open, err := os.Open("./testdata/signed-tx.json")
-	require.NoError(t, err)
-
-	bz, err := ioutil.ReadAll(open)
+	bz, err := ioutil.ReadFile("./testdata/signed-tx.json")
 	require.NoError(t, err)
 
 	var stdTx auth.StdTx
