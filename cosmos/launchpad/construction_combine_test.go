59211e98baa8504f8a4e5372bc8bf1da22b63820
sahith-narahari <sahithnarahari@gmail.com>
2020-09-11 03:03:13 +0530

    add signatures to test

--- cosmos/launchpad/construction_combine_test.go
+++ cosmos/launchpad/construction_combine_test.go
@@ -35,7 +35,11 @@ func TestLaunchpad_ConstructionCombine(t *testing.T) {
 
 	// base64 encode the encoded tx bytes
 	txBytesBase64 := base64.StdEncoding.EncodeToString(txBytes)
-
+	var signBytes []byte
+	for _, sign := range stdTx.Signatures {
+		signBytes = append(signBytes[:], sign.Bytes()...)
+	}
+	t.Log(txBytesBase64)
 	adapter := NewLaunchpad(TendermintAPI{}, CosmosAPI{}, properties)
 	var combineRes, combineErr = adapter.ConstructionCombine(context.Background(), &types.ConstructionCombineRequest{
 		UnsignedTransaction: txBytesBase64,
@@ -43,8 +47,17 @@ func TestLaunchpad_ConstructionCombine(t *testing.T) {
 			SigningPayload: &types.SigningPayload{
 				Address: "cosmos1qrv8g4hwt4z6ds8mednhhgx907wug9d6y8n9jy",
 				Bytes:   []byte("KlPOI6frSRdjVHgiBIIpHI2PAQQnCCMSTWgonJQbECRATe8Yf7gqRgAVeLcPiVUbp2oSq2P7pp51f0iCtiA47Q==")},
+			PublicKey: &types.PublicKey{
+				CurveType: types.Secp256k1,
+				Bytes:     []byte("AjWp/hwbxozAbAXZNxP0NXDiAy/9k8FfHr1hbVbunnUV"),
+			},
+			SignatureType: types.Ecdsa,
+			Bytes:         signBytes,
 		},
 		}})
 	require.Nil(t, combineErr)
 	require.NotNil(t, combineRes)
+	t.Log(combineRes.SignedTransaction)
+	err = cdc.UnmarshalJSON(bz, &stdTx)
+	t.Log(stdTx.Signatures)
 }
