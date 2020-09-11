07cacf6c38254e116ad65caa082cd66a0aef20bb
Jonathan Gimeno <jgimeno@gmail.com>
2020-09-11 18:58:48 +0200

    Add commit.

--- cosmos/launchpad/construction_submit.go
+++ cosmos/launchpad/construction_submit.go
@@ -2,9 +2,14 @@ package launchpad
 
 import (
 	"context"
-	"encoding/base64"
+	"encoding/hex"
 	"fmt"
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
 	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/x/auth/types"
 	cosmosclient "github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk/generated"
 
 	"github.com/coinbase/rosetta-sdk-go/types"
@@ -13,20 +18,21 @@ import (
 )
 
 func (l Launchpad) ConstructionSubmit(ctx context.Context, req *types.ConstructionSubmitRequest) (*types.TransactionIdentifierResponse, *types.Error) {
-	bz, err := base64.StdEncoding.DecodeString(req.SignedTransaction)
+	bz, err := hex.DecodeString(req.SignedTransaction)
 	if err != nil {
 		return nil, rosetta.WrapError(ErrInvalidTransaction, "error decoding tx")
 	}
 
-	var stdTx cosmosclient.StdTxValue
 	cdc := simapp.MakeCodec()
-	err = cdc.UnmarshalJSON(bz, &stdTx)
+
+	var stdTx sdk.StdTx
+	err = cdc.UnmarshalBinaryLengthPrefixed(bz, &stdTx)
+
 	txBroadcast := cosmosclient.InlineObject{
-		Tx: cosmosclient.StdTx{
-			Value: stdTx,
-		},
-		Mode: "async",
+		Tx:   mapStdTxToApiStdTx(stdTx),
+		Mode: "block",
 	}
+
 	resp, _, err := l.cosmos.Transactions.TxsPost(ctx, txBroadcast)
 	if err != nil {
 		return nil, rosetta.WrapError(ErrNodeConnection, fmt.Sprintf("error broadcasting tx: %s", err))
@@ -37,4 +43,53 @@ func (l Launchpad) ConstructionSubmit(ctx context.Context, req *types.Constructi
 			Hash: resp.Hash,
 		},
 	}, nil
+}
+
+func mapStdTxToApiStdTx(stdTx sdk.StdTx) cosmosclient.StdTx {
+	clientStdTx := cosmosclient.StdTx{Value: cosmosclient.StdTxValue{
+		Memo: stdTx.Memo,
+	}}
+
+	for _, msg := range stdTx.Msgs {
+		sendTxMsg := msg.(bank.MsgSend)
+
+		var amount []cosmosclient.Coin
+		for _, c := range sendTxMsg.Amount {
+			amount = append(amount, cosmosclient.Coin{
+				Denom:  c.Denom,
+				Amount: c.Amount.String(),
+			})
+		}
+
+		m := cosmosclient.Msg{
+			Type: msg.Type(),
+			Value: cosmosclient.MsgValue{
+				FromAddress: sendTxMsg.FromAddress.String(),
+				ToAddress:   sendTxMsg.ToAddress.String(),
+				Amount:      amount,
+			},
+		}
+
+		clientStdTx.Value.Msg = append(clientStdTx.Value.Msg, m)
+	}
+
+	for _, s := range stdTx.Signatures {
+		clientStdTx.Value.Signatures = append(clientStdTx.Value.Signatures, cosmosclient.StdTxValueSignatures{
+			Signature: hex.EncodeToString(s.Signature),
+		})
+	}
+
+	var fees []cosmosclient.Coin
+	for _, c := range stdTx.Fee.Amount {
+		clientStdTx.Value.Fee.Amount = append(clientStdTx.Value.Fee.Amount, cosmosclient.Coin{
+			Denom:  c.Denom,
+			Amount: c.Amount.String(),
+		})
+	}
+	clientStdTx.Value.Fee = cosmosclient.StdTxValueFee{
+		Gas:    strconv.Itoa(int(stdTx.GetGas())),
+		Amount: fees,
+	}
+
+	return clientStdTx
 }
--- cosmos/launchpad/construction_submit_test.go
+++ cosmos/launchpad/construction_submit_test.go
@@ -1,67 +1,61 @@
 package launchpad
 
 import (
-	"context"
-	"encoding/base64"
 	"encoding/hex"
-	"fmt"
-	"github.com/stretchr/testify/mock"
-	cosmosclient "github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk/generated"
-	"github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk/mocks"
 	"io/ioutil"
 	"testing"
 
-	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/stretchr/testify/require"
-
-	"github.com/tendermint/cosmos-rosetta-gateway/rosetta"
 )
 
 func TestLaunchpad_ConstructionSubmit(t *testing.T) {
-	expectedHash := "6f22ea7620ebcb5078d244f06e88dd26906ba1685135bfc34f83fefdd653198a"
-
 	bz, err := ioutil.ReadFile("./testdata/test-with-signature-delete.json")
 	require.NoError(t, err)
 
-	var stdTx cosmosclient.StdTxValue
 	cdc := simapp.MakeCodec()
+
+	var stdTx sdk.StdTx
 	err = cdc.UnmarshalJSON(bz, &stdTx)
 	require.NoError(t, err)
-
-	testTx := cosmosclient.InlineObject{
-		Tx: cosmosclient.StdTx{
-			Value: stdTx,
-		},
-		Mode: "async",
-	}
-
-	m := mocks.CosmosTransactionsAPI{}
-	m.
-		On("TxsPost", mock.Anything, testTx).
-		Return(cosmosclient.BroadcastTxCommitResult{
-			Hash:   expectedHash,
-			Height: 10,
-		}, nil, nil).Once()
-
-	adapter := NewLaunchpad(TendermintAPI{}, CosmosAPI{Transactions: &m}, rosetta.NetworkProperties{})
 
 	// re-encode it via the Amino wire protocol
 	txBytes, err := cdc.MarshalBinaryLengthPrefixed(stdTx)
 	require.NoError(t, err)
 
 	toString := hex.EncodeToString(txBytes)
-	fmt.Printf("\n%s\n", toString)
+	t.Logf("%s\n", toString)
 
-	// base64 encode the encoded tx bytes
-	txBytesBase64 := base64.StdEncoding.EncodeToString(txBytes)
-
-	resp, err2 := adapter.ConstructionSubmit(context.Background(), &types.ConstructionSubmitRequest{
-		SignedTransaction: txBytesBase64,
-	})
-	fmt.Printf("%v\n", resp)
-
-	require.Nil(t, err2)
-	require.NotNil(t, resp)
-	require.Equal(t, expectedHash, resp.TransactionIdentifier.Hash)
+	//testTx := cosmosclient.InlineObject{
+	//	Tx: cosmosclient.StdTx{
+	//		//Value: stdTx,
+	//	},
+	//	Mode: "async",
+	//}
+	//
+	//m := mocks.CosmosTransactionsAPI{}
+	//m.
+	//	On("TxsPost", mock.Anything, testTx).
+	//	Return(cosmosclient.BroadcastTxCommitResult{
+	//		Hash:   expectedHash,
+	//		Height: 10,
+	//	}, nil, nil).Once()
+	//
+	//adapter := NewLaunchpad(TendermintAPI{}, CosmosAPI{Transactions: &m}, rosetta.NetworkProperties{})
+	//
+	//// re-encode it via the Amino wire protocol
+	//txBytes, err := cdc.MarshalBinaryLengthPrefixed(stdTx)
+	//require.NoError(t, err)
+	//
+	//toString := hex.EncodeToString(txBytes)
+	//
+	//resp, err2 := adapter.ConstructionSubmit(context.Background(), &types.ConstructionSubmitRequest{
+	//	SignedTransaction: toString,
+	//})
+	//fmt.Printf("%v\n", resp)
+	//
+	//require.Nil(t, err2)
+	//require.NotNil(t, resp)
+	//require.Equal(t, expectedHash, resp.TransactionIdentifier.Hash)
 }
