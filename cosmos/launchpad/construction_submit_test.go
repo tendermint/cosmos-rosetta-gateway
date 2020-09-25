811dd5a6808755e661552a0f0787539c077b7e03
Jonathan Gimeno <jgimeno@gmail.com>
2020-09-25 15:06:22 +0200

    Change test.

--- cosmos/launchpad/construction_submit_test.go
+++ cosmos/launchpad/construction_submit_test.go
@@ -3,9 +3,10 @@ package launchpad
 import (
 	"context"
 	"encoding/hex"
-	"fmt"
 	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk/mocks"
@@ -14,34 +15,18 @@ import (
 	"github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk"
 	"github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/tendermint"
 
-	"github.com/cosmos/cosmos-sdk/x/auth/client/rest"
-
-	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLaunchpad_ConstructionSubmit(t *testing.T) {
-	bz, err := ioutil.ReadFile("./testdata/test-with-signature-delete.json")
+	bz, err := ioutil.ReadFile("./testdata/signed-tx.json")
 	require.NoError(t, err)
-
-	cdc := simapp.MakeCodec()
-
-	var stdTx rest.BroadcastReq
-	err = cdc.UnmarshalJSON(bz, &stdTx)
-	require.NoError(t, err)
-
-	// re-encode it via the Amino wire protocol
-	txBytes, err := cdc.MarshalJSON(stdTx)
-	require.NoError(t, err)
-	t.Logf("%s\n", txBytes)
-
-	toString := hex.EncodeToString(txBytes)
-	t.Logf("%s\n", toString)
+	toString := hex.EncodeToString(bz)
 
 	expectedHash := "6f22ea7620ebcb5078d244f06e88dd26906ba1685135bfc34f83fefdd653198a"
 	m := &mocks.SdkClient{}
 	m.
-		On("PostTx", context.Background(), bz).
+		On("PostTx", context.Background(), mock.Anything).
 		Return(cosmostypes.TxResponse{
 			TxHash: expectedHash,
 			Height: 10,
@@ -56,7 +41,6 @@ func TestLaunchpad_ConstructionSubmit(t *testing.T) {
 	resp, err2 := adapter.ConstructionSubmit(context.Background(), &types.ConstructionSubmitRequest{
 		SignedTransaction: toString,
 	})
-	fmt.Printf("%v\n", resp)
 
 	require.Nil(t, err2)
 	require.NotNil(t, resp)
