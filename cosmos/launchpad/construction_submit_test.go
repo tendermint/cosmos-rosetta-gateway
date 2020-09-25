0698a4e99412479c5b356406a9ca4b94ff595469
SaReN <sahithnarahari@gmail.com>
2020-09-25 19:56:35 +0530

    Fix submit (#111)
    * fix submit
    
    Signed-off-by: sahith-narahari <sahithnarahari@gmail.com>
    
    * Change test.
    
    * Fix the test.
    
    * Update signed tx.
    
    Co-authored-by: Jonathan Gimeno <jgimeno@gmail.com>

--- cosmos/launchpad/construction_submit_test.go
+++ cosmos/launchpad/construction_submit_test.go
@@ -3,7 +3,7 @@ package launchpad
 import (
 	"context"
 	"encoding/hex"
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"testing"
 
@@ -14,34 +14,30 @@ import (
 	"github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/sdk"
 	"github.com/tendermint/cosmos-rosetta-gateway/cosmos/launchpad/client/tendermint"
 
-	"github.com/cosmos/cosmos-sdk/x/auth/client/rest"
-
-	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLaunchpad_ConstructionSubmit(t *testing.T) {
-	bz, err := ioutil.ReadFile("./testdata/test-with-signature-delete.json")
+	bz, err := ioutil.ReadFile("./testdata/signed-tx.json")
+	require.NoError(t, err)
+	signedTx := hex.EncodeToString(bz)
+
+	var tx map[string]json.RawMessage
+	err = json.Unmarshal(bz, &tx)
 	require.NoError(t, err)
 
-	cdc := simapp.MakeCodec()
+	bReq := BroadcastReq{
+		Tx:   tx["value"],
+		Mode: "block",
+	}
 
-	var stdTx rest.BroadcastReq
-	err = cdc.UnmarshalJSON(bz, &stdTx)
+	byteRequest, err := json.Marshal(bReq)
 	require.NoError(t, err)
-
-	// re-encode it via the Amino wire protocol
-	txBytes, err := cdc.MarshalJSON(stdTx)
-	require.NoError(t, err)
-	t.Logf("%s\n", txBytes)
-
-	toString := hex.EncodeToString(txBytes)
-	t.Logf("%s\n", toString)
 
 	expectedHash := "6f22ea7620ebcb5078d244f06e88dd26906ba1685135bfc34f83fefdd653198a"
 	m := &mocks.SdkClient{}
 	m.
-		On("PostTx", context.Background(), bz).
+		On("PostTx", context.Background(), byteRequest).
 		Return(cosmostypes.TxResponse{
 			TxHash: expectedHash,
 			Height: 10,
@@ -54,9 +50,8 @@ func TestLaunchpad_ConstructionSubmit(t *testing.T) {
 
 	adapter := newAdapter(m, tendermint.NewClient(""), properties)
 	resp, err2 := adapter.ConstructionSubmit(context.Background(), &types.ConstructionSubmitRequest{
-		SignedTransaction: toString,
+		SignedTransaction: signedTx,
 	})
-	fmt.Printf("%v\n", resp)
 
 	require.Nil(t, err2)
 	require.NotNil(t, resp)
